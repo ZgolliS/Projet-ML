878985852e017094eafb31caab5aa9ff92f9824b
Amine Ktari <amine.gtari@esprit.tn>
2022-04-11 09:21:29 +0200

    projet ML

---
+++ Projet/consumer.py
@@ -0,0 +1,14 @@
+from kafka import KafkaConsumer
+import json
+
+
+c = KafkaConsumer('test', bootstrap_servers=['kafka:9093'], api_version=(2,6,0))
+
+def process_msg(msg):
+    print(msg.offset)
+    print(json.loads(msg.value))
+
+
+for msg in c:
+    process_msg(msg)
+
